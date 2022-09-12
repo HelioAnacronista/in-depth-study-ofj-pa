068aac3594c52799e85fb8911eab9ad0eb0dd68f
Helio <heliojobs25@gmail.com>
2022-09-12 18:16:50 -0300

    1. Start project in-depth study of jpa

---
+++ src/main/java/io/gitgub/helioanacronista/springbootjpadeep/SpringBootJpaDeepApplication.java
@@ -0,0 +1,13 @@
+package io.gitgub.helioanacronista.springbootjpadeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaDeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaDeepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/gitgub/helioanacronista/springbootjpadeep/SpringBootJpaDeepApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitgub.helioanacronista.springbootjpadeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaDeepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
